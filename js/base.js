1c56f8d62fe59fdb6db0ce86883fc0abc6ffd9f7
Trevor Hunsaker <saturdayplace@gmail.com>
2012-01-11 00:25:48 -0700

    Fixed some typos in '2nd in command' post.  Updated footonotes js.  Works a bit nicer now.  Some tweaks to the footnotes CSS.

--- js/base.js
+++ js/base.js
@@ -1,18 +1,38 @@
 $(document).ready(function(){
 	// Footnote system (js and css) ripped off from the Django Advent (djangoadvent.com)
 	$("#footnotes").hide();
-	
-	$(".footnote-reference").one('click', function(){
-		var note_selector = "[id="+ $(this).attr("href").split("#")[1] + "]";
+
+	// Move the copy from the <ol> at the bottom to an <aside> element
+	// directly beneath the relevant <p>, then hide.  Assign a click handler
+	// to each footnote's reference link.
+	$(".footnote-reference").each(function(index){
+		var $this = $(this);
+		var note_selector = "[id=" + (index + 1)+ "]";
 		var note = $(note_selector).parent().text().replace("↑", "");
-		var aside = $("<aside>").text(note);
-		$(this).parent().after(aside);
-		aside.hide().slideDown();
+		var p = $this.parent();
+
+		var aside = $("<aside>").text(note).attr("name", $(this).attr("href") );
+		p.after(aside);
+		aside.hide();
 		
-		$(this).click(function(){
+		// Assign a click handler for toggling the footnotes.
+		$this.click(function(){
 			aside.slideToggle(400);
+			return false;
 		});
 		
-		return false;
 	});
+
+	// Display and jump to the footnote if someone arrives at a page via a # url
+	if(window.location.hash){
+		var hash = window.location.hash;
+		var aside_selector = "aside[name=" + hash + "]";
+		var aside = $(aside_selector);
+		aside.show();
+
+		$('html, body').animate({
+			scrollTop: aside.offset().top - 20
+		}, 1000);
+	}
+	
 });
